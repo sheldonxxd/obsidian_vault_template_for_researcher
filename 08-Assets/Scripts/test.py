d7390115016c1e41d9ed0fb7d9c4cf01cd03ac66
sheldonxxd <sheldon_sinap@163.com>
2022-05-10 11:22:09 +0800

    增加测试日志输出

--- 08-Assets/Scripts/test.py
+++ 08-Assets/Scripts/test.py
@@ -1,10 +1,27 @@
-from obs import CrossRef
+import logging
+
+logging.basicConfig(level=logging.DEBUG #设置日志输出格式
+                    ,filename="test.log" #log日志输出的文件位置和文件名
+                    ,filemode="w" #文件的写入格式，w为重新写入文件，默认是追加
+                    ,format="%(asctime)s - %(name)s - %(levelname)-9s - %(filename)-8s : %(lineno)s line - %(message)s" #日志输出的格式
+                    # -8表示占位符，让输出左对齐，输出长度都为8位
+                    ,datefmt="%Y-%m-%d %H:%M:%S" #时间输出的格式
+                    )
+
+############## 测试某个功能 ###################
+
+from crossref import CrossRef
 
 def test(doi):
     a = CrossRef(doi)
     print(a.get_title())
 
 
+##############################################
+
 if __name__=='__main__':
-    doi = '10.1021/acs.analchem.0c01074'
-    test(doi)
+    try:
+        doi = '10.1021/acs.fflctem.0c01074'  # 一个无效的doi
+        test(doi)
+    except Exception as e:
+        logging.exception(e)
